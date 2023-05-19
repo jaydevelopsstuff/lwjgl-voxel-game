401dad5ee9b634ec9b68622ea3d47ce5b68f28f1
jaydevelopsstuff <70743392+jaydevelopsstuff@users.noreply.github.com>
2023-05-19 11:30:56 -0700

    Create Player and ClientPlayer classes

---
+++ src/main/java/net/jay/voxelgame/entity/player/ClientPlayer.java
@@ -0,0 +1,22 @@
+package net.jay.voxelgame.entity.player;
+
+import net.jay.voxelgame.render.gl.Camera;
+
+public class ClientPlayer extends Player {
+    private Camera camera;
+
+    public ClientPlayer() {
+        super();
+        this.camera = new Camera();
+    }
+
+    public Camera camera() {
+        return camera;
+    }
+
+    @Override
+    public void setPos(double x, double y, double z) {
+        super.setPos(x, y, z);
+        camera.setPos(x, y, z);
+    }
+}
---
+++ src/main/java/net/jay/voxelgame/entity/player/Player.java
@@ -0,0 +1,21 @@
+package net.jay.voxelgame.entity.player;
+
+import org.joml.Vector3f;
+
+public class Player {
+    private Vector3f pos;
+
+    public Player() {
+        this.pos = new Vector3f();
+    }
+
+    public Vector3f pos() {
+        return pos;
+    }
+
+    public void setPos(double x, double y, double z) {
+        pos.x = (float)x;
+        pos.y = (float)y;
+        pos.z = (float)z;
+    }
+}
